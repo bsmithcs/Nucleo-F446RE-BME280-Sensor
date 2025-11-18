57843afd214f8e735ebec92d84ec87788b2d1783
Brayden Smith <bray7den@gmail.com>
2025-11-18 14:01:48 -0500

    Working Serial Monitor and NeoPixels

--- src/main.cpp
+++ src/main.cpp
@@ -8,49 +8,84 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <SPI.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_NeoPixel.h>
+#include "stm32f4xx.h"
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+#ifndef SSD_ARRAY_H
+#define SSD_ARRAY_H
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+#ifdef __cplusplus
+extern "C" {
+    #endif
+    void SSD_init(void);
+    void SSD_update(int digitSelect, int value, int decimalPoint);
+    #ifdef __cplusplus
+}
+#endif
+#endif
 
-#define PIXEL_PORT  GPIOA
-#define PIXEL_PIN   0 
+#define PIXEL_PIN   PA0 
 #define NUMPIXELS   4 
 
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
-Adafruit_NeoPixel pixels(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+#define BTN_PIN     PC13
 
+#define WHITE pixels.Color(150, 150, 150)
+#define RED pixels.Color(150, 0, 0)
+#define PURPLE pixels.Color(150, 0, 150)
+#define BLUE pixels.Color(0, 150, 150)
+#define GREEN pixels.Color(0, 150, 0)
+
+#define BAUDRATE 115200
+#define BME_ADDRESS 0x76
+
+Adafruit_BME280 bme; // I2C
+Adafruit_NeoPixel pixels(NUMPIXELS, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 /**************** Variables ****************/
 
-unsigned long delayTime;
+// State
+typedef enum state {
+    temp_C, temp_F, humidity, pressure
+} state_e;
+volatile state_e current_state;
+
+// SSD State
+volatile uint32_t digit_select;
+volatile uint32_t decimal;
+
+// Readings
+volatile float tempC_reading;
+volatile float tempF_reading;
+volatile float humidity_reading;
+volatile float pressure_reading;
+volatile float SSD_reading;
 
 /**************** Declarations ****************/
 
-void print_values(void);
+void TIM2_Handler(void);
+void BTN_Handler(void);
+
+void printValues(void);
+void update_reading(void);
+void update_state(void);
 
 /**************** Setup ****************/
 
 void setup() {
-    Serial.begin(9600);
-    while(!Serial);    // time to get serial running
-    Serial.println(F("BME280 test"));
 
+    /***** Setup Serial Monitor *****/
+    Serial.begin(BAUDRATE);
+    while(!Serial);    // time to get serial running
+    Serial.println(F("\nCPEG222 Project 5 - BME280 Values\n"));
+
+    /***** Setup BME Sensor *****/
     unsigned status;
-    
-    // default settings
-    status = bme.begin();  
-    // You can also pass in a Wire library object like &Wire2
-    // status = bme.begin(0x76, &Wire2)
+    status = bme.begin(BME_ADDRESS);  
     if (!status) {
         Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
         Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
@@ -61,26 +96,37 @@ void setup() {
         while (1) delay(10);
     }
     
-    Serial.println("-- Default Test --");
-    delayTime = 1000;
-
-    Serial.println();
+    /***** Setup Pixels *****/
     pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+    pixels.setBrightness(50);
+    pixels.clear(); // Set all pixel colors to 'off'
+    pixels.fill(RED);
+    pixels.show();
+
+    /***** Setup SSD *****/
+    SSD_init();
+
+    /***** Setup BTN *****/
+    pinMode(BTN_PIN, INPUT); // Set Button as input
+    attachInterrupt(BTN_PIN, BTN_Handler, FALLING); // Button interrupt
+
+    /***** Setup TIM2 *****/
+    TIM2->PSC = 15; // Prescaler: (16MHz/(15+1) = 1MHz, 1usec period)
+    TIM2->ARR = 5000 - 1; // Auto-reload when CNT = XX: (period = XX usec)
+    TIM2->CR1 = TIM_CR1_CEN; // Enable TIM2
+    attachInterrupt(TIM2_IRQn, TIM2_Handler, RISING);
+
+    /***** Initialize State Variables *****/
+    current_state = temp_C;
+
+    Serial.println("Successfully Configured");
 }
 
 /**************** Loop ****************/
 
 void loop() { 
-    pixels.clear(); // Set all pixel colors to 'off'
-
-    for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-      pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-      pixels.show();   // Send the updated pixel colors to the hardware.
-      delay(500); // Pause before next pass through loop
-    }
-
     printValues();
-    delay(delayTime);
+    delay(5000);
 }
 
 /**************** Definitions ****************/
@@ -88,20 +134,88 @@ void loop() {
 void printValues() {
     Serial.print("Temperature = ");
     Serial.print(bme.readTemperature());
-    Serial.println(" °C");
+    Serial.print(" °C\t");
+
+    Serial.print("Temperature = ");
+    Serial.print((bme.readTemperature() * (9.0f/5.0f)) + 32.0f);
+    Serial.print(" °F\t");
+
+    Serial.print("RelHum = ");
+    Serial.print(bme.readHumidity());
+    Serial.print(" %\t");
 
     Serial.print("Pressure = ");
-
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
-
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
-
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
+    Serial.print((bme.readPressure() / 100.0f) * 0.00098692f, 4);
+    Serial.println(" atm");
 
     Serial.println();
+}
+
+void update_state(void) {
+    pixels.clear();
+    switch (current_state) {
+        case temp_C:
+            current_state = temp_F;
+            pixels.fill(RED);
+            break;
+        case temp_F:
+            current_state = humidity;
+            pixels.fill(PURPLE);
+            break;
+        case humidity:
+            current_state = pressure;
+            pixels.fill(BLUE);
+            break;
+        case pressure:
+            current_state = temp_C;
+            pixels.fill(GREEN);
+            break;
+        default:
+            break;
+    }
+    pixels.show();
+}
+
+void update_reading(void) {
+
+    tempC_reading = bme.readTemperature();
+    tempF_reading = tempC_reading * (9/5) + 32;
+    humidity_reading = bme.readHumidity();
+    pressure_reading = bme.readPressure();
+
+    switch (current_state) {
+        case temp_C:
+            SSD_reading = tempC_reading * 100;
+            break;
+        case temp_F:
+            SSD_reading = tempF_reading * 100;
+            break;
+        case humidity:
+            SSD_reading = humidity_reading * 100;
+            break;
+        case pressure:
+            SSD_reading = pressure_reading * 10;
+            break;
+        default:
+            break;
+    }
+}
+
+/// @brief Handles getting IR reading and interpreting it, updating current reading.
+/// Updates SSD with IR_reading. Triggered by TIM2 interrupt every 500 us (0.5 ms).
+void TIM2_Handler(void){
+
+    digit_select = (digit_select + 1) % 4;
+
+    update_reading();
+
+    if (current_state == pressure) decimal = 1;
+    else decimal = 2;
+
+    SSD_update(digit_select, SSD_reading, decimal);
+}
+ 
+/// @brief Handles button presses to update the what measurement to display (state)
+void BTN_Handler(void) {
+    update_state();
 }
